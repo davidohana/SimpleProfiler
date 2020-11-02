51e33f6ad0c4abcfdb5a21b16dad63f6fa7106ca
davidoh <david.ohana@ibm.com>
2020-11-02 10:26:51 +0200

    docs

--- python/sample_app.py
+++ python/sample_app.py
@@ -7,7 +7,9 @@ from simple_profiler import SimpleProfiler
 
 print("app started")
 
-profiler = SimpleProfiler(report_sec=10, enclosing_section_name="total", reset_after_sample_count=500000)
+profiler = SimpleProfiler(report_sec=10,
+                          enclosing_section_name="total",
+                          reset_after_sample_count=500000)
 
 profiler.start_section("init")
 payload = os.urandom(10000)
